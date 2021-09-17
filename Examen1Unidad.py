e8b0af2238efc5a24917a28605aa3f172ce57a90
Isaac <L211050222@pabellon.tecnm.mx>
2021-09-17 13:45:13 -0500

    Commit examen

---
+++ Examen1Unidad.py
@@ -0,0 +1,11 @@
+#Datos del alumno
+print("Nombre: Isaac Jared Reyna González \n Lugar de residencia: Rincón de Romos \n Lugar de nacimientio: Aguascalientes \n")
+print("Color favorito: Morado \n Animal favorito: Zorro \n \n ")
+#PREGUNTAS
+print("PREGUNTAS \n")
+print("¿Qué es la programación? \n La programación es el proceso utilizado para ordenar las acciones necesarias para realizar un proyecto.\n")
+print("\n ¿Por qué es importante la programación? \n La programación es importante porque es el medio que permite interactuar con los proyectos para crear funciones que pueden llegar a ser revolucionarios y permitir un avance tecnológico. \n")
+print("\n Explique con sus palabras que es herencia. \n La herencia es la capacidad de un objeto de heredar las características de su clase, por ejemeplo: una lavadora hereda las características de los electrodomesticos de usar la electricidad para funcionar. \n")
+print("\n Explique con sus palabras que es poliformismo. \n El polimorfismo es cuando una acción se repite en varios objetos, pero no se realiza de la misma forma, por ejemplo: abrir una puerta no es lo mismo que abrir una ventana. \n  ")
+print(" \n Explique con sus palabras que es abstracción. \n Las abstracción es cuando a un objeto le quitas todas aquellas acciones que no necesita, pero se le dejan aquellas que son necesarias y utiles \n ")
+print(" \n Explique con sus palabras que es encapsulamiento. \n El encapsulamiento es aislar una parte del código como un objeto o una acción, para que el resto del código y el usario no sea capaz de interactuar con este")
