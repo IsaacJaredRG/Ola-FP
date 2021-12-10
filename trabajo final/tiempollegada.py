ad00108fa4ea817c6feb8090a67bc76735d663f7
Isaac Jared <90349219+IsaacJaredRG@users.noreply.github.com>
2021-12-10 17:08:18 -0600

    Delete trabajo final directory

--- trabajo final/tiempollegada.py
+++
@@ -1,8 +0,0 @@
-#Programa que te estime el tiempo de llegada a un destino con base en la velocidad y distancia.
-a=str(input("¿cual es su destino de llegada? "))
-d=int(input ("¿cual es la distancia desde su locacion hasta su destino? "))
-v=int(input ("¿cual es la velocidad en km/h? "))
-t=d/v
-print ("el tiempo de llegada estimado es: ",t, "horas")
-
-	
